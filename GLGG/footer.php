0bb6474eae5eb04f33fee7de32f1c38913c9bb57
MozLee <lixin@mozlee.com>
2018-02-10 17:44:04 +0800

    Ver1.02
    提交新版本文件

---
+++ GLGG/footer.php
@@ -0,0 +1,17 @@
+		<footer>
+			<div class="footer">
+				<div class="footCon">
+					<a href="#">©2017 sy1x</a>/
+					<a href="http://typecho.org/">Powered by Typecho </a>/
+					<a href="http://sy1x.com/index.php/archives/6.html">Theme 'Galegaygay' by Sy1x </a>/
+					<a href="https://www.aliyun.com">阿里云强力驱动中</a>
+					<a href="#">辽ICP备17012102号</a>
+				</div>
+			</div>
+		</footer>
+		<a class="goTop hide">咻~</a>
+	</body>
+<script type="text/javascript" src="<?php $this->options->themeUrl('js/fun.js'); ?>" ></script>
+<script src="//cdn.bootcss.com/highlight.js/9.1.0/highlight.min.js"></script>
+<script>hljs.initHighlightingOnLoad();</script>
+</html>
